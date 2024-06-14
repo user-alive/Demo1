a1e0bf231f752b12b4465bee54988f91d2ec4fdc
Rajani Paudel <rajanipaudel35@gmail.com>
2024-06-14 09:12:16 +0545

    my job is to multiply

--- calculator_app.py
+++ calculator_app.py
@@ -6,8 +6,12 @@ input_number2= int(print("Enter another number"))
 
 #Create a function called subtract_number
 
-#Create a function called multiply_number
+def multiply_number(input_number1, input_number2):
+    return input_number1 * input_number2
+
 
 #Create a function called divide_number
+
+
 
 
