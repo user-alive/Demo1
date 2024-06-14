b94170a46bec13b8e51b402efc52cf7f83a0200a
Anuska Grg <anuskagrg218@gmail.com>
2024-06-14 09:06:35 +0545

    subtracted number

--- calculator_app.py
+++ calculator_app.py
@@ -2,9 +2,15 @@
 input_number1 = int(input("Enter a number"))
 input_number2= int(print("Enter another number"))
 
+print(subtract_number(input_number1, input_number2))
+
 #Create a function called add_number
 
+
 #Create a function called subtract_number
+def subtract_number(input_number1, input_number2):
+    return input_number1 - input_number2
+
 
 #Create a function called multiply_number
 
