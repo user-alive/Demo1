38c110863ff0482be07050ecaae6b96e08bcab75
SantoshDhakal <clab2080@gmail.com>
2024-06-14 08:34:37 +0545

    Created a python file called calculator_app

---
+++ calculator_app.py
@@ -0,0 +1,13 @@
+
+input_number1 = int(input("Enter a number"))
+input_number2= int(print("Enter another number"))
+
+#Create a function called add_number
+
+#Create a function called subtract_number
+
+#Create a function called multiply_number
+
+#Create a function called divide_number
+
+
