d3dd2a84b419179ca8e2dc32ebf96e22f4cde229
Jayanti Gurung <jayantigurung686@gmail.com>
2024-06-14 09:05:28 +0545

    Added two numbers

--- calculator_app.py
+++ calculator_app.py
@@ -2,12 +2,15 @@
 input_number1 = int(input("Enter a number"))
 input_number2= int(print("Enter another number"))
 
+print(input_number1, input_number2)
+
 #Create a function called add_number
+def add_number(input_number1, input_number2):
+    return input_number1 + input_number2
 
 #Create a function called subtract_number
 
 #Create a function called multiply_number
 
 #Create a function called divide_number
-
 
