3f213c199e954bbf1687c96ca5cf5070da0d03b0
Aaki Chhetri <aakichhetri@gmail.com>
2024-06-14 09:07:05 +0545

    Added a divide module

--- calculator_app.py
+++ calculator_app.py
@@ -4,10 +4,13 @@ input_number2= int(print("Enter another number"))
 
 #Create a function called add_number
 
-#Create a function called subtract_number
+
+    return input_number1  input_number2
 
 #Create a function called multiply_number
 
 #Create a function called divide_number
+def divide_number(input_number1, input_number2):
+    return input_number1 / input_number2
 
 
